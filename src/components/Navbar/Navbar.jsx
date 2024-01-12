59958a45f8439db4bbfd8550306377b93b99350b
arushmano <61136045+Arush16101999@users.noreply.github.com>
2024-01-12 21:00:29 +0530

    fixed navbar issue

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -13,19 +13,19 @@ const Navbar = () => {
         <img src={images.gericht} alt="app__logo" />
       </div>
       <ul className="app__navbar-links">
-        <li className="p_opensans">
+        <li className="p__opensans">
           <a href="#home">Home</a>
         </li>
-        <li className="p_opensans">
+        <li className="p__opensans">
           <a href="#about">About</a>
         </li>
-        <li className="p_opensans">
+        <li className="p__opensans">
           <a href="#menu">Menu</a>
         </li>
-        <li className="p_opensans">
+        <li className="p__opensans">
           <a href="#awards">Awards</a>
         </li>
-        <li className="p_opensans">
+        <li className="p__opensans">
           <a href="#contact">Contact</a>
         </li>
       </ul>
@@ -38,8 +38,9 @@ const Navbar = () => {
           Reserve Table
         </a>
       </div>
-      <div className="app_navbar-smallscreen_overlay flex__center slide-bottom">
+      <div className="app__navbar-smallscreen">
         <GiHamburgerMenu
+          color="#fff"
           fontSize={27}
           className="overlay__close"
           onClick={() => setToggleMenu(true)}
